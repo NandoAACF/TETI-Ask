8b4db6acc8dea0089bd005364d5ac9247dfce4f9
NandoAACF <nando.aacf88@gmail.com>
2024-03-06 11:39:36 +0700

    feat: add backend endpoint for faq crud

--- backend/app.py
+++ backend/app.py
@@ -13,6 +13,7 @@ client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["TETI-Ask"]
 col_documents = db["Documents"]
 col_users = db["Users"]
+col_faq = db["FAQ"]
 
 @app.route('/document', methods=['POST'])
 def add_document():
@@ -94,13 +95,23 @@ def update_document(id):
     else:
         return not_found()
 
-@app.route('/documents/category/<category>', methods=['GET'])
-def documents_filter(category):
+@app.route('/documents/category/verified/<category>', methods=['GET'])
+def documents_filter_verified(category):
     documents = col_documents
     if category == 'all':
         documents = documents.find({'status': 'verified'})
     else:
         documents = documents.find({'category': category, 'status': 'verified'})
+    resp = dumps(documents)
+    return resp
+
+@app.route('/documents/category/unverified/<category>', methods=['GET'])
+def documents_filter_unverified(category):
+    documents = col_documents
+    if category == 'all':
+        documents = documents.find({'status': 'unverified'})
+    else:
+        documents = documents.find({'category': category, 'status': 'unverified'})
     resp = dumps(documents)
     return resp
     
@@ -140,7 +151,101 @@ def login_admin():
         resp = jsonify('Invalid email!')
         resp.status_code = 400
         return resp
+    
+@app.route('/faq', methods=['POST'])
+def add_faq():
+    _json = request.json
+    _question = _json['question']
+    _answer = _json['answer']
+    _category = _json['category']
+    _status = _json['status']
 
+    if _question and _answer and request.method == 'POST':
+        col_faq.insert_one({'question': _question, 'answer': _answer, 'category': _category, 'status': _status})
+        resp = jsonify('FAQ added successfully!')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
+    
+@app.route('/faqs', methods=['GET'])
+def faqs():
+    faqs = col_faq.find()
+    resp = dumps(faqs)
+    return resp
+
+@app.route('/faq/<id>', methods=['GET'])
+def faq(id):
+    faq = col_faq.find_one({'_id': ObjectId(id)})
+    resp = dumps(faq)
+    return resp
+
+@app.route('/faqs/unverified', methods=['GET'])
+def faqs_unverified():
+    faqs = col_faq.find({'status': 'unverified'})
+    resp = dumps(faqs)
+    return resp
+
+@app.route('/faqs/verified', methods=['GET'])
+def faqs_verified():
+    faqs = col_faq.find({'status': 'verified'})
+    resp = dumps(faqs)
+    return resp
+
+@app.route('/faq/verify/<id>', methods=['PUT'])
+def verify_faq(id):
+    _id = id
+    if _id and request.method == 'PUT':
+        col_faq.update_one({'_id': ObjectId(_id)}, {'$set': {'status': "verified"}})
+        resp = jsonify('FAQ verified successfully!')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
+
+@app.route('/faq/<id>', methods=['DELETE'])
+def delete_faq(id):
+    col_faq.delete_one({'_id': ObjectId(id)})
+    resp = jsonify('FAQ deleted successfully!')
+    resp.status_code = 200
+    return resp
+
+@app.route('/faq/<id>', methods=['PUT'])
+def update_faq(id):
+    _id = id
+    _json = request.json
+    _question = _json['question']
+    _answer = _json['answer']
+    _category = _json['category']
+    _status = _json['status']
+
+    if _question and _answer and _id and request.method == 'PUT':
+        col_faq.update_one({'_id': ObjectId(_id['$oid']) if '$oid' in _id else ObjectId(_id)}, {'$set': {'question': _question, 'answer': _answer, 'category': _category, 'status': _status}})
+        resp = jsonify('FAQ updated successfully!')
+        resp.status_code = 200
+        return resp
+    else:
+        return not_found()
+    
+@app.route('/faqs/category/verified/<category>', methods=['GET'])
+def faqs_filter_verified(category):
+    faqs = col_faq
+    if category == 'all':
+        faqs = faqs.find({'status': 'verified'})
+    else:
+        faqs = faqs.find({'category': category, 'status': 'verified'})
+    resp = dumps(faqs)
+    return resp
+
+@app.route('/faqs/category/unverified/<category>', methods=['GET'])
+def faqs_filter_unverified(category):
+    faqs = col_faq
+    if category == 'all':
+        faqs = faqs.find({'status': 'unverified'})
+    else:
+        faqs = faqs.find({'category': category, 'status': 'unverified'})
+    resp = dumps(faqs)
+    return resp
 
 @app.errorhandler(404)
 def not_found(error=None):
