0d1e8fcf973e26b3d530ea6b0180ad39f82cf77d
NandoAACF <nando.aacf88@gmail.com>
2024-03-06 17:08:35 +0700

    fix: fixing card document admin responsiveness

--- frontend/src/components/CardDocumentAdmin.js
+++ frontend/src/components/CardDocumentAdmin.js
@@ -6,18 +6,20 @@ export default function CardDocumentAdmin({
     link = "https://docs.google.com/document/d/1uyeYHZDYmIRauOLuYesSFHFc-BqG7iA7gn4t7qnmuhk/edit?usp=sharing",
 }) {
     return (
-        <div className="flex flex-col items-center justify-between w-[259px] sm:w-[279px] min-h-[170px] pb-[20px] bg-white rounded-[10px] shadow-md relative">
-            <div className="flex flex-col items-start justify-start w-full">
-                <div className="w-full h-[30px] rounded-t-[10px] bg-red-600"></div>
-                <div className="flex flex-col items-start justify-start w-full px-[13px] mt-[10px]">
-                    <h4 className="text-[20px] font-semibold text-black ">{title}</h4>
-                    <div className="flex flex-col items-start justify-start w-full mt-[8px] gap-[8px] flex-wrap">
-                        <div className="flex flex-col items-center justify-center px-[7px] py-[2px] rounded-md bg-green-400">
-                            <p className="text-[12px] text-black font">{category}</p>
+        <div className="flex flex-col items-center justify-between w-[259px] sm:w-[279px] sm:h-[290px] pb-[20px] bg-white rounded-[10px] shadow-md relative">
+            <div className="flex flex-col items-start justify-between w-full h-full">
+                <div className="flex flex-col items-start justify-start w-full">
+                    <div className="w-full h-[30px] rounded-t-[10px] bg-red-600"></div>
+                    <div className="flex flex-col items-start justify-start w-full px-[13px] mt-[10px]">
+                        <h4 className="text-[20px] font-semibold text-black ">{title}</h4>
+                        <div className="flex flex-col items-start justify-start w-full mt-[8px] gap-[8px] flex-wrap">
+                            <div className="flex flex-col items-center justify-center px-[7px] py-[2px] rounded-md bg-green-400">
+                                <p className="text-[12px] text-black font">{category}</p>
+                            </div>
                         </div>
+                        <h5 className="text-[14px] text-gray-500 mt-[8px]">{date}</h5>
+                        <h5 className="text-[14px] font-normal text-black mt-[6px]">{description}</h5>
                     </div>
-                    <h5 className="text-[14px] text-gray-500 mt-[8px]">{date}</h5>
-                    <h5 className="text-[14px] font-normal text-black mt-[6px]">{description}</h5>
                 </div>
                 <div className="flex flex-col items-start justify-start w-full px-[13px]">
                     <a
