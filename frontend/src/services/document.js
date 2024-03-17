c19853519d76c5d7d19c919862ac1bd9dcdc28df
Nc Galih <encegalih@gmail.com>
2024-03-17 11:16:49 +0700

    fix: refactor use api

--- frontend/src/services/document.js
+++ frontend/src/services/document.js
@@ -1,39 +1,36 @@
 import { useEffect, useState } from "react";
 import api, { useRefetch } from "./api";
 
-export const useGetVerifiedDocument = () => {
+export const useGetDocument = (status = "verified") => {
     const [docs, setDocs] = useState([]);
     const [category, setCategory] = useState("");
-    const [search, setSearch] = useState("")
-    useEffect(() => {
-        const endpoint = category
-            ? `/documents/category/verified/${category}`
-            : "/documents/verified"
-        api.get(endpoint)
-            .then((res) => setDocs(res.data))
-            .catch(console.log);
-    }, [category]);
-    const data = !search ? docs : docs.filter(doc => 
-        doc.title.toLowerCase().includes(search.toLowerCase()) ||
-        doc.description.toLowerCase().includes(search.toLowerCase()) 
-    )
-    return { data, setCategory, setSearch };
-};
-
-export const useGetUnverifiedDocument = () => {
-    const [docs, setDocs] = useState([]);
-    const [category, setCategory] = useState("");
+    const [search, setSearch] = useState("");
     const {onRefetch, refetch} = useRefetch();
+
     useEffect(() => {
         const endpoint = category
-            ? `/documents/category/unverified/${category}`
-            : "/documents/unverified"
+            ? `/documents/category/${status}/${category}`
+            : `/documents/${status}`
         api.get(endpoint)
             .then((res) => setDocs(res.data))
             .catch(console.log);
     }, [onRefetch, category]);
-    return { data: docs, refetch, setCategory };
+
+    const data = !search ? docs : docs.filter(doc => 
+        doc.title.toLowerCase().includes(search.toLowerCase()) ||
+        doc.description.toLowerCase().includes(search.toLowerCase()) 
+    );
+    
+    return { data, refetch, setCategory, setSearch };
 };
+
+export const useGetUnverifiedDocument = () => {
+    return useGetDocument("unverified");
+}
+
+export const useGetVerifiedDocument = () => {
+    return useGetDocument("verified");
+}
 
 export const postDocument = async (document) => {
     const payload = {
--- frontend/src/services/faqs.js
+++ frontend/src/services/faqs.js
@@ -1,39 +1,36 @@
 import { useEffect, useState } from "react";
 import api, { useRefetch } from "./api";
 
-export const useGetVerifiedFaqs = () => {
+export const useGetFaqs = (status = "verified") => {
     const [faqs, setFaqs] = useState([]);
     const [category, setCategory] = useState("");
     const [search, setSearch] = useState("");
+    const {onRefetch, refetch} = useRefetch();
+
     useEffect(() => {
         const endpoint = category
-            ? `/faqs/category/verified/${category}`
-            : "/faqs/verified"
+            ? `/faqs/category/${status}/${category}`
+            : `/faqs/${status}`
         api.get(endpoint)
             .then((res) => setFaqs(res.data))
             .catch(console.log);
-    }, [category]);
+    }, [category, onRefetch]);
+
     const data = !search ? faqs : faqs.filter(faq => 
         faq.question.toLowerCase().includes(search.toLowerCase()) ||
         faq.answer.toLowerCase().includes(search.toLowerCase()) 
     );
-    return { data, setCategory, setSearch };
+
+    return { data, refetch, setCategory, setSearch };
 };
 
 export const useGetUnverifiedFaqs = () => {
-    const [faqs, setFaqs] = useState([]);
-    const [category, setCategory] = useState("");
-    const {onRefetch, refetch} = useRefetch();
-    useEffect(() => {
-        const endpoint = category
-            ? `/faqs/category/unverified/${category}`
-            : "/faqs/unverified"
-        api.get(endpoint)
-            .then((res) => setFaqs(res.data))
-            .catch(console.log);
-    }, [onRefetch, category]);
-    return { data: faqs, refetch, setCategory };
+    return useGetFaqs("unverified");
 };
+
+export const useGetVerifiedFaqs = () => {
+    return useGetFaqs("verified");
+}
 
 export const postFaq = async (faq) => {
     const payload = {
