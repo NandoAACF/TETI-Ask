014c876cf02bf074214bce56c14be145ddcca43a
NandoAACF <nando.aacf88@gmail.com>
2024-03-17 16:05:57 +0700

    fix: refactor backend code

--- backend/app.py
+++ backend/app.py
@@ -13,8 +13,6 @@ app = Flask(__name__)
 
 CORS(app)
 
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/TETI-Ask'
-
 client = pymongo.MongoClient(os.getenv("MONGO_URI") )
 db = client["TETI-Ask"]
 col_documents = db["Documents"]
