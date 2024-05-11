9583381caf9de76fad3e8b8eab8a97eae0c9ae67
Nc Galih <encegalih@gmail.com>
2024-05-11 15:22:41 +0700

    fix: error

--- frontend/src/components/DocumentCategory.js
+++ frontend/src/components/DocumentCategory.js
@@ -8,7 +8,7 @@ export default function SelectDocumentCategory({handleCategory}){
             className="bg-white border-[1px] border-slate-300 rounded-[10px] w-[125px] sm:w-[150px] py-[5px] px-[7px] mt-[2px] hover:bg-white cursor-pointer outline-none transition-all ease-in-out duration-200"
         >
             <option value="">All</option>
-            {category.map(item => <option value={item}>{item}</option>)}
+            {category.map(item => <option value={item} key={item}>{item}</option>)}
         </select>
     )
 }
