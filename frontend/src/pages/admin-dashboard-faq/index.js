267d616e9fc1f3bfb9612ec965cf6ec2177679fd
Nc Galih <encegalih@gmail.com>
2024-05-11 14:37:02 +0700

    fix: option category

--- frontend/src/pages/admin-dashboard-faq/index.js
+++ frontend/src/pages/admin-dashboard-faq/index.js
@@ -11,6 +11,7 @@ import { useGetUnverifiedFaqs } from "@/services/faqs";
 import { useProtectedRoute } from "@/services/admin";
 import Head from "next/head";
 import Link from "next/link";
+import SelectFaqCategory from "@/components/FaqCategory";
 
 export default function AdminDashboardFAQ() {
     useProtectedRoute();
@@ -33,14 +34,7 @@ export default function AdminDashboardFAQ() {
                 <div className="flex flex-col md:flex-row items-start md:items-center justify-start mt-[20px] gap-x-[20px] gap-y-[20px] md:gap-x-[35px] md:gap-y-[25px] md:flex-wrap">
                     <div className="flex flex-row items-center justify-start gap-[15px]">
                         <h4 className="text-[17px] font-medium text-left">Category:</h4>
-                        <select
-                            onChange={handleCategory}
-                            className="bg-white border-[1px] border-slate-300 rounded-[10px] w-[125px] sm:w-[150px] py-[5px] px-[7px] mt-[2px] hover:bg-white cursor-pointer outline-none transition-all ease-in-out duration-200"
-                        >
-                            <option value="">All</option>
-                            <option value="akademik">Akademik</option>
-                            <option value="organisasi">Organisasi</option>
-                        </select>
+                        <SelectFaqCategory handleCategory={handleCategory} />
                     </div>
                     <div className="flex flex-row items-center justify-start gap-[15px]">
                         <h4 className="text-[17px] font-medium text-left">Search:</h4>
